8123921621efde307b1516ce09d599744239cd4d
Jeidencw <henriquecw22@gmail.com>
2023-09-19 12:00:12 -0300

    try

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const button = document.querySelector('button')
-const lis = document.querySelectorAll('li')
-const ul = document.querySelector('ul')
-
-
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+const lis = document.querySelectorAll('li');
 
 button.addEventListener('click', () => {
-    const li = document.createElement('li')
+    const li = document.createElement('li');
+    li.textContent = 'Novo item';
+    ul.append(li);
 
-    li.textContent = 'Novo item'
-    ul.append(li)
-})
+    const lisArray = Array.from(lis);
 
-lis.forEach(li => {
-    li.addEventListener('click', event => {
-        const clickedElement = event.target
-        const verificaEstilo = clickedElement.style.textDecoration === 'line-through'
+    let juntaTudo = lisArray.concat(li)
 
-        console.log(verificaEstilo);
+    test(juntaTudo);
+});
 
-        if(verificaEstilo){
-            clickedElement.style.textDecoration = ''
-        }else{
-            clickedElement.style.textDecoration = 'line-through'
-        }
-    })
-})
+const test = (arr) => {
+    arr.forEach((li) => {
+        li.addEventListener('click', (event) => {
+            const clickedElement = event.target;
+            const verificaEstilo = clickedElement.style.textDecoration === 'line-through';
+
+            console.log(verificaEstilo);
+
+            if (verificaEstilo) {
+                clickedElement.style.textDecoration = '';
+            } else {
+                clickedElement.style.textDecoration = 'line-through';
+            }
+        });
+    });
+};
+
+test(lis);
